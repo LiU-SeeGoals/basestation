0e08e87eb20cae7e316eff7dd153cb0533bb7253
Axel <axel.hammarberg@gmail.com>
2023-12-06 16:15:04 +0100

    Updated proto generate

--- Libraries/protobuf/generate.bat
+++ Libraries/protobuf/generate.bat
@@ -1,12 +1,18 @@
 @echo off
 @setlocal
 
-pushd proto
+pushd %~dp0
+
+copy %~dp0..\..\proto-messages\*.proto .
 
 for %%A in (*.proto) do (
-	call %~dp0..\..\..\nanopb\generator\nanopb_generator.bat %%A
-	move %%~dpnA.pb.c %~dp0..\Src
-	move %%~dpnA.pb.h %~dp0..\Inc
+	call %~dp0..\..\nanopb\generator\nanopb_generator.bat %%A
+	move %%~dpnA.pb.c %~dp0Src
+	move %%~dpnA.pb.h %~dp0Inc
+)
+
+for %%A in (*.proto) do (
+	del %%A
 )
 
 popd
